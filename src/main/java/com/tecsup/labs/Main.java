d60ed93a60ab4e58c900ce28e5cc8350595c5fdb
AlonsoSam <alonso.alejo@tecsup.edu.pe>
2025-11-21 23:26:39 -0500

    feat: Se corrigen todos los errores de Checkstyle, BUILD SUCCESS

--- src/main/java/com/tecsup/labs/Main.java
+++ src/main/java/com/tecsup/labs/Main.java
@@ -1,13 +1,35 @@
 package com.tecsup.labs;
-public class Main {
-public static void main(String[] args) {
-UserRegistrationService service = new UserRegistrationService();
-// Casos de prueba simples
-service.registerUser("juan", "123", "juan@correo"); // Contraseña muy corta
-System.out.println(service.lastErrorMessage);
-service.registerUser(null, "12345678", "correo-sin-arroba"); // username null
-System.out.println(service.lastErrorMessage);
-service.registerUser("error", "12345678", "error@correo.com"); // fuerza excepción
-System.out.println(service.lastErrorMessage);
-}
+
+/**
+ * Clase principal para probar el servicio de registro de usuarios.
+ */
+public final class Main {
+
+    /**
+     * Constructor privado para evitar la instanciación de la clase utility.
+     */
+    private Main() {
+        // Clase utility.
+    }
+
+    /**
+     * Método principal de la aplicación.
+     * @param args Argumentos de la línea de comandos.
+     */
+    public static void main(final String[] args) {
+        final UserRegistrationService service = new UserRegistrationService();
+
+        // Casos de prueba simples
+        service.registerUser("juan", "123", "juan@correo.com");
+        System.out.println("Error 1: " + service.getLastErrorMessage());
+
+        service.registerUser(null, "12345678", "correo@valido.com");
+        System.out.println("Error 2: " + service.getLastErrorMessage());
+
+        service.registerUser("error", "12345678", "error@correo.com");
+        System.out.println("Error 3: " + service.getLastErrorMessage());
+
+        System.out.println("Longitud de 'Test': "
+                            + service.getStringLength("Test"));
+    }
 }
